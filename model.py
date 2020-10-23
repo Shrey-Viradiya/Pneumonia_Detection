56036b4a65ca3e4927273f41395561a73eb2c05f
Shrey Viradiya <40735908+Shrey-Viradiya@users.noreply.github.com>
2020-10-23 17:06:08 +0530

    Model Class Completed

--- model.py
+++ model.py
@@ -21,9 +21,7 @@ class CoronaDetection():
 
     Idea is to use transfer Learning
     """
-    def __init__(self, data_directory):
-
-        self.data_directory = data_directory
+    def __init__(self):
 
         if os.path.exists('./model/ConvModel'):
             self.model = torch.load('./model/ConvModel')
@@ -39,3 +37,89 @@ class CoronaDetection():
                 torch.nn.Linear(500, 2)
             )
             torch.save('./model/ConvModel')
+
+    def train(self, optimizer, loss_fun, train_data ,test_data, epochs = 20, device = 'cuda'):
+        '''
+        Train function:
+        parameters:
+        model       : PyTorch Model
+        optimizer   : optimizer object
+        loss_fun    : Loss Function object
+        train_data  : CIFAR10 train dataloader
+        test_data   : CIFAR10 test  dataloader
+        batch_size  : default value 100
+        epochs      : default value 20
+        device      : 'cuda' or 'cpu', default 'cuda'
+        '''
+
+        max_accurracy = 0.0
+
+        for epoch in range(epochs):
+            start = time.time()
+
+            training_loss = 0.0
+            valid_loss = 0.0
+
+            self.model.train()
+            correct = 0 
+            total = 0
+            for batch in train_data:
+                train_images, train_labels = batch
+                train_images = train_images.to(device)
+                train_labels = train_labels.to(device)
+
+                optimizer.zero_grad()
+                output = self.model(train_images)
+                loss = loss_fun(output, train_labels)
+                loss.backward()
+                optimizer.step()
+                training_loss += loss.item()
+                _, predicted = torch.max(output.data, 1)
+                total += train_labels.size(0)
+                correct += (predicted == train_labels).sum().item()
+            training_accuracy = correct/total * 100
+
+            self.model.eval()
+            correct = 0 
+            total = 0
+            with torch.no_grad():
+                for batch in test_data:
+                    test_images, test_labels = batch
+                    test_images = test_images.to(device)
+                    test_labels = test_labels.to(device)
+
+                    output = self.model(test_images)
+                    loss = loss_fun(output,test_labels) 
+                    valid_loss += loss.item()
+                    _, predicted = torch.max(output.data, 1)
+                    total += test_labels.size(0)
+                    correct += (predicted == test_labels).sum().item()
+            testing_accuracy = correct/total * 100
+
+            if (testing_accuracy > max_accurracy):
+                    max_accurracy = testing_accuracy
+                    torch.save(self.model, './model/ConvModel')
+
+            print(f'{bcolors.OKGREEN}Epoch:{bcolors.ENDC} {epoch + 1}, {bcolors.OKGREEN}Training Loss:{bcolors.ENDC} {training_loss:.5f}, {bcolors.OKGREEN}Validation Loss:{bcolors.ENDC} {valid_loss:.5f}, {bcolors.OKGREEN}Training accuracy:{bcolors.ENDC} {training_accuracy:.2f} %, {bcolors.OKGREEN}Testing accuracy:{bcolors.ENDC} {testing_accuracy:.2f} %, {bcolors.OKGREEN}time:{bcolors.ENDC} {time.time() - start:.2f} s')
+
+    def test(self, loss_fun, test_data, device = 'cuda'):
+        start = time.time()
+        self.model.eval()
+        test_loss = 0.0
+        correct = 0 
+        total = 0
+        with torch.no_grad():
+            for batch in test_data:
+                test_images, test_labels = batch
+                test_images = test_images.to(device)
+                test_labels = test_labels.to(device)
+
+                output = self.model(test_images)
+                loss = loss_fun(output,test_labels) 
+                test_loss += loss.item()
+                _, predicted = torch.max(output.data, 1)
+                total += test_labels.size(0)
+                correct += (predicted == test_labels).sum().item()
+        testing_accuracy = correct/total * 100
+
+        print(f'{bcolors.OKGREEN}Test Loss:{bcolors.ENDC} {test_loss:.5f}, {bcolors.OKGREEN}Testing accuracy:{bcolors.ENDC} {testing_accuracy:.2f} %, {bcolors.OKGREEN}time:{bcolors.ENDC} {time.time() - start:.2f} s')
