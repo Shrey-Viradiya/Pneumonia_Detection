e87a0c0a702ebff3cb411112dca857321a34a1bb
Shrey Viradiya <40735908+Shrey-Viradiya@users.noreply.github.com>
2020-10-23 21:49:42 +0530

    Little Change

--- main.py
+++ main.py
@@ -19,12 +19,19 @@ batch_size=32
 train_data_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=True)
 test_data_loader  = torch.utils.data.DataLoader(test_data  , batch_size=batch_size, shuffle=True)
 
+
+
 if torch.cuda.is_available():
+    print("Using GPU")
     device = torch.device("cuda") 
 else:
+    print("Using CPU")
     device = torch.device("cpu")
 
+print("Creating Model Object: ")
 model = CoronaDetection()
 optimizer = torch.optim.Adam(model.model.parameters(), lr=0.00005)
 
+print("Starting Training")
 model.train(optimizer, torch.nn.CrossEntropyLoss(), train_data_loader, test_data_loader, epochs=25, device=device)
+print("Completed Training")
