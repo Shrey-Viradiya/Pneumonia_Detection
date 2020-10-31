ac48b968ce2c4236a7611d013a1bf709d39caa8b
Shrey Viradiya <40735908+Shrey-Viradiya@users.noreply.github.com>
2020-11-01 02:25:42 +0530

    Normalization added

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ img_transforms = torchvision.transforms.Compose([
     torchvision.transforms.RandomCrop((64,64)),
     torchvision.transforms.RandomHorizontalFlip(),
     torchvision.transforms.ToTensor(),
+    torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
     ])
 
 print("Setting up Data Directories")
