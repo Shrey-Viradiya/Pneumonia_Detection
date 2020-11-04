99e4cf03a2dbc709ab6d8405fdc48641db36d647
Tirth Patel <tirthasheshpatel@gmail.com>
2020-11-04 16:00:24 +0530

    run black for beautiful code. a ma mi ya
    Former-commit-id: c89fb654b1093c8b5a8f01043b8ee2ccb41c5336

--- test.py
+++ test.py
@@ -5,15 +5,19 @@ import sys
 
 if torch.cuda.is_available():
     print("Using GPU")
-    device = torch.device("cuda") 
+    device = torch.device("cuda")
 else:
     print("Using CPU")
     device = torch.device("cpu")
 
 test_data_path = sys.argv[1]
-test_data = torchvision.datasets.ImageFolder(root=test_data_path,transform=img_test_transforms)
-batch_size=32
-test_data_loader  = torch.utils.data.DataLoader(test_data  , batch_size=batch_size, shuffle=True)
+test_data = torchvision.datasets.ImageFolder(
+    root=test_data_path, transform=img_test_transforms
+)
+batch_size = 32
+test_data_loader = torch.utils.data.DataLoader(
+    test_data, batch_size=batch_size, shuffle=True
+)
 
 model = CoronaDetection(sys.argv[2])
-model.test(torch.nn.CrossEntropyLoss(), test_data_loader, device = device)
+model.test(torch.nn.CrossEntropyLoss(), test_data_loader, device=device)
