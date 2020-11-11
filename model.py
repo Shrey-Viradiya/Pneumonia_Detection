afdf0ca5a4fe5ade6cbbefef7d0252533cbe4272
Shrey Viradiya <40735908+Shrey-Viradiya@users.noreply.github.com>
2020-11-11 12:17:22 +0530

    history is saved as it is generated
    Former-commit-id: c29f3a2b72ba37f11a2a9689c296488113833084

--- model.py
+++ model.py
@@ -13,9 +13,15 @@ import skimage.transform
 # Dictionary for pretrained models and their last layer name
 pretrained_models = {
     "ResNet18": [torchvision.models.resnet18, "layer4"],
+    "ResNet34": [torchvision.models.resnet34, "layer4"],
     "ResNet50": [torchvision.models.resnet50, "layer4"],
+    "ResNet101": [torchvision.models.resnet101, "layer4"],
+    "ResNet152": [torchvision.models.resnet152, "layer4"],
     "Alexnet": [torchvision.models.alexnet, "features"],
+    "VGG11": [torchvision.models.vgg11_bn, "features"],
+    "VGG13": [torchvision.models.vgg13_bn, "features"],
     "VGG16": [torchvision.models.vgg16_bn, "features"],
+    "VGG19": [torchvision.models.vgg19_bn, "features"],
     "GoogleNet": [torchvision.models.googlenet, "inception5b"],
     "Inception": [torchvision.models.inception_v3, "Mixed_7c"],
 }
@@ -67,9 +73,15 @@ class CoronaDetection:
     def __init__(self, base_model="ResNet18"):
         assert base_model in [
             "ResNet18",
+            "ResNet34",
             "ResNet50",
+            "ResNet101",
+            "ResNet152",
             "Alexnet",
+            "VGG11",
+            "VGG13",
             "VGG16",
+            "VGG19",
             "GoogleNet",
             "Inception",
         ]
@@ -140,149 +152,147 @@ class CoronaDetection:
         train_accuracies = []
         test_accuracies = []
 
-        try:
-            for epoch in range(epochs):
-                start = time.time()
+        for epoch in range(epochs):
+            start = time.time()
 
-                training_loss = 0.0
-                valid_loss = 0.0
+            training_loss = 0.0
+            valid_loss = 0.0
 
-                train_correct = 0
-                train_total = 0
+            train_correct = 0
+            train_total = 0
 
-                # Training over batches
-                self.model.train(mode=True)
-                for train_batch in train_data:
-                    train_images, train_labels = train_batch
-                    train_images = train_images.to(device)
-                    train_labels = train_labels.to(device)
+            # Training over batches
+            self.model.train(mode=True)
+            for train_batch in train_data:
+                train_images, train_labels = train_batch
+                train_images = train_images.to(device)
+                train_labels = train_labels.to(device)
 
-                    optimizer.zero_grad()
-                    train_output = self.model(train_images)
+                optimizer.zero_grad()
+                train_output = self.model(train_images)
 
-                    if self.base_model == "Inception":
-                        train_output = train_output.logits
+                if self.base_model == "Inception":
+                    train_output = train_output.logits
 
-                    train_loss = loss_fun(train_output, train_labels)
+                train_loss = loss_fun(train_output, train_labels)
 
-                    train_loss.backward()
-                    optimizer.step()
+                train_loss.backward()
+                optimizer.step()
 
-                    training_loss += train_loss.item()
+                training_loss += train_loss.item()
 
-                    _, train_predicted = torch.max(train_output.data, 1)
+                _, train_predicted = torch.max(train_output.data, 1)
 
-                    train_total += train_labels.size(0)
-                    train_ccount = (train_predicted == train_labels).sum().item()
-                    train_correct += train_ccount
+                train_total += train_labels.size(0)
+                train_ccount = (train_predicted == train_labels).sum().item()
+                train_correct += train_ccount
 
-                    sys.stdout.write(
-                        f"\rEpoch {epoch+1}\t"
-                        f"Training Loss => {train_loss:.4f}\t"
-                        f"Training Acc => "
-                        f"{train_ccount/train_images.shape[0]*100:5.2f}"
-                    )
-
-                training_accuracy = train_correct / train_total * 100
-
-                valid_loss = 0.0
-                test_correct = 0
-                test_total = 0
-                misses = 0
-                previous_accuracy = 0
-
-                # Test over batches
-                self.model.train(mode=False)
-                with torch.no_grad():
-                    for test_batch in test_data:
-                        test_images, test_labels = test_batch
-                        test_images = test_images.to(device)
-                        test_labels = test_labels.to(device)
-
-                        test_output = self.model(test_images)
-
-                        if self.base_model == "Inception":
-                            test_output = test_output.logits
-
-                        test_loss = loss_fun(test_output, test_labels)
-                        valid_loss += test_loss.item()
-
-                        _, test_predicted = torch.max(test_output.data, 1)
-
-                        test_total += test_labels.size(0)
-                        test_ccount = (test_predicted == test_labels).sum().item()
-                        test_correct += test_ccount
-
-                testing_accuracy = test_correct / test_total * 100
-
-                sys.stdout.flush()
-                sys.stdout.write("\r")
-
-                time_taken = time.time() - start
-
-                print(
-                    f"Epoch {epoch + 1}\t"
-                    f"Training Loss => {training_loss:.4f}\t"
-                    f"Training Acc => {training_accuracy:5.2f}\t"
-                    f"Test Loss => {valid_loss:.4f}\t"
-                    f"Test Acc => {testing_accuracy:5.2f}\t"
-                    f"Time Taken => {time_taken:5.2f}"
+                sys.stdout.write(
+                    f"\rEpoch {epoch+1}\t"
+                    f"Training Loss => {train_loss:.4f}\t"
+                    f"Training Acc => "
+                    f"{train_ccount/train_images.shape[0]*100:5.2f}"
                 )
 
-                train_losses.append(training_loss)
-                test_losses.append(valid_loss)
-                train_accuracies.append(training_accuracy)
-                test_accuracies.append(testing_accuracy)
+            training_accuracy = train_correct / train_total * 100
 
-                # Save if it is better model than max_accuracy
-                if testing_accuracy > max_accurracy:
-                    max_accurracy = testing_accuracy
-                    torch.save(self.model, f"./model/ConvModel_{self.base_model}")
+            valid_loss = 0.0
+            test_correct = 0
+            test_total = 0
+            misses = 0
+            previous_accuracy = 0
 
-                    with open(
-                        f"./model/ConvModel_{self.base_model}_results.txt", "w"
-                    ) as f:
-                        f.writelines(
-                            [
-                                f"BaseModel: {self.base_model}\n",
-                                f"Epochs: {epoch + 1}\n",
-                                f"Train Dataloader Batch Size: {train_data.batch_size}\n",
-                                f"Test Dataloader Batch Size: {test_data.batch_size}\n",
-                                f'Params for Adam: {optimizer.__dict__["defaults"]}\n',
-                                f"Training Loss: {training_loss}\n",
-                                f"Validation Loss: {valid_loss}\n",
-                                f"Training Accuracy: {training_accuracy}\n",
-                                f"Testing Accuracy: {testing_accuracy}\n",
-                                f"Time Taken: {time_taken} seconds",
-                            ]
-                        )
+            # Test over batches
+            self.model.train(mode=False)
+            with torch.no_grad():
+                for test_batch in test_data:
+                    test_images, test_labels = test_batch
+                    test_images = test_images.to(device)
+                    test_labels = test_labels.to(device)
 
-                # Decide and stop early if needed
-                if epoch >= 1:
-                    if (
-                        previous_accuracy > testing_accuracy
-                        and misses < early_stopping_threshold
-                    ):
-                        misses += 1
-                        previous_accuracy = testing_accuracy
-                    elif previous_accuracy > testing_accuracy:
-                        print(f"Early Stopping....")
-                        print(
-                            f"Epoch {epoch + 1}\t"
-                            f"Training Loss => {training_loss:.4f}\t"
-                            f"Training accuracy => {training_accuracy:5.2f}\t"
-                            f"Test Loss => {valid_loss:.4f}\t"
-                            f"Testing accuracy => {testing_accuracy:5.2f}\t"
-                            f"Time Taken => {time_taken:.2f}"
-                        )
-                        break
-                previous_accuracy = testing_accuracy
-        except KeyboardInterrupt:
-            print()
-            print("Force Stopping Model Training...")
-            return train_losses, train_accuracies, test_losses, test_accuracies
+                    test_output = self.model(test_images)
 
-        return train_losses, train_accuracies, test_losses, test_accuracies
+                    if self.base_model == "Inception":
+                        test_output = test_output.logits
+
+                    test_loss = loss_fun(test_output, test_labels)
+                    valid_loss += test_loss.item()
+
+                    _, test_predicted = torch.max(test_output.data, 1)
+
+                    test_total += test_labels.size(0)
+                    test_ccount = (test_predicted == test_labels).sum().item()
+                    test_correct += test_ccount
+
+            testing_accuracy = test_correct / test_total * 100
+
+            sys.stdout.flush()
+            sys.stdout.write("\r")
+
+            time_taken = time.time() - start
+
+            print(
+                f"Epoch {epoch + 1}\t"
+                f"Training Loss => {training_loss:.4f}\t"
+                f"Training Acc => {training_accuracy:5.2f}\t"
+                f"Test Loss => {valid_loss:.4f}\t"
+                f"Test Acc => {testing_accuracy:5.2f}\t"
+                f"Time Taken => {time_taken:5.2f}"
+            )
+
+            train_losses.append(training_loss)
+            test_losses.append(valid_loss)
+            train_accuracies.append(training_accuracy)
+            test_accuracies.append(testing_accuracy)
+
+            # Save if it is better model than max_accuracy
+            if testing_accuracy > max_accurracy:
+                max_accurracy = testing_accuracy
+                torch.save(self.model, f"./model/ConvModel_{self.base_model}")
+
+                with open(
+                    f"./model/ConvModel_{self.base_model}_results.txt", "w"
+                ) as f:
+                    f.writelines(
+                        [
+                            f"BaseModel: {self.base_model}\n",
+                            f"Epochs: {epoch + 1}\n",
+                            f"Train Dataloader Batch Size: {train_data.batch_size}\n",
+                            f"Test Dataloader Batch Size: {test_data.batch_size}\n",
+                            f'Params for Adam: {optimizer.__dict__["defaults"]}\n',
+                            f"Training Loss: {training_loss}\n",
+                            f"Validation Loss: {valid_loss}\n",
+                            f"Training Accuracy: {training_accuracy}\n",
+                            f"Testing Accuracy: {testing_accuracy}\n",
+                            f"Time Taken: {time_taken} seconds",
+                        ]
+                    )
+
+            # Decide and stop early if needed
+            if epoch >= 1:
+                if (
+                    previous_accuracy > testing_accuracy
+                    and misses < early_stopping_threshold
+                ):
+                    misses += 1
+                    previous_accuracy = testing_accuracy
+                elif previous_accuracy > testing_accuracy:
+                    print(f"Early Stopping....")
+                    print(
+                        f"Epoch {epoch + 1}\t"
+                        f"Training Loss => {training_loss:.4f}\t"
+                        f"Training accuracy => {training_accuracy:5.2f}\t"
+                        f"Test Loss => {valid_loss:.4f}\t"
+                        f"Testing accuracy => {testing_accuracy:5.2f}\t"
+                        f"Time Taken => {time_taken:.2f}"
+                    )
+                    break
+            previous_accuracy = testing_accuracy
+
+            np.save(f"./model/train_losses_{self.model.base_model}", train_losses)
+            np.save(f"./model/train_accuracies_{self.model.base_model}", train_accuracies)
+            np.save(f"./model/test_losses_{self.model.base_model}", test_losses)
+            np.save(f"./model/test_accuracies_{self.model.base_model}", test_accuracies)
 
     def test(self, loss_fun, test_data, device="cuda"):
         print("Starting Evaluating....")
